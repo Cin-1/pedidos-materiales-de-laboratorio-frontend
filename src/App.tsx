84e07f00cdab7426116284929697d6b01e1cf234
miuds22 <marcosu.desousa@gmail.com>
2024-10-27 21:01:01 -0300

    feat/Request view

--- src/App.tsx
+++ src/App.tsx
@@ -24,62 +24,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index path="/login" element={<Login />} />
-          
-          <Route index path="/pedidos" element={
-          <div>
-              <Header 
-              title = {"pedidos"}
-              enableSearch = {true}
-              icon = {"request.svg"}
-              backArrow = {false}
-              searchPlaceholder = {"Buscar Pedidos"}    
-            ></Header>
-
-            <div  className="body">
-            <CardRequest
-            title = {"Bioquimica comision 25 -  Clase 6"}
-            date  = {"24/7/1993"}
-            laboratory ={""}
-            building  ={""}
-            proffesor  ={""}
-            students  ={"Carlos Lombardi"}
-            >
-           </CardRequest>              
-
-            <CardRequest
-            title = {"Bioquimica comision 25 -  Clase 7"}
-            date  = {"24/7/1993"}
-            laboratory ={""}
-            building  ={""}
-            proffesor  ={""}
-            students  ={"Carlos Lombardi"}
-            ></CardRequest>
-
-            
-            <MobileNav>
-              
-            </MobileNav>
-            </div>
-          </div>
-
-
-          } />
-
-          <Route
-            index
-            path="/test"
-            element={
-              <CardRequest
-                title="Pedido #1"
-                banner="Pendiente"
-                date="26/04/06"
-                laboratory="65"
-                building="Malvinas"
-                proffesor="Cin"
-                students="kamakdn"
-              />
-            }
-          />
 
           {/* Component PrivateRoute will check for a valid JWT
            * and redirect to '/login' if there isn't one
--- src/views/requests/index.tsx
+++ src/views/requests/index.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement } from "react";
 import "./styles.scss";
 import Header from "../../components/header";
+import CardRequest from "../../components/card";
+import MobileNav from "../../components/mobile-nav";
 
 export default function RequestsView(): ReactElement {
 
@@ -16,10 +18,36 @@ export default function RequestsView(): ReactElement {
     searchCallback: onSearchResult
   }
 
-  return <>
-      <Header {...headerAttributes}></Header>
-      <main>
+  const requesteds  = [{
+    title : "Bioquimica comision 25"
+    ,date : "24/7/1993"
+    ,laboratory :""
+    ,building :""
+    ,proffesor : ""
+    ,students : "Carlos Lombardi"
+    
+  },
+{
+    title : "Bioquimica comision 25"
+    ,date : "24/7/1993"
+    ,laboratory :""
+    ,building :""
+    ,proffesor : ""
+    ,students : "Carlos Lombardi"
+    
+  }]
 
+  return <>
+    <Header {...headerAttributes}></Header>
+    <main>
+      <div  className="body">
+              {requesteds.map((requested,index) =>
+                <div className="listElements">
+                    <CardRequest {... requested} />  
+                </div>  
+              )}
+        </div>     
       </main>
+      <MobileNav></MobileNav>
     </>;
 }
